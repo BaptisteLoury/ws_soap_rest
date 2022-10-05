467b2142e70693802fc1ded69e7dd3c9aa3b4d84
David <fredidas@hotmail.com>
2022-10-05 12:21:52 +0200

    db

--- TrainFilteringWS/create.sql
+++ TrainFilteringWS/create.sql
@@ -4,55 +4,43 @@ USE trains;
 
 DROP TABLE IF EXISTS TRAINS;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 CREATE TABLE `trains` (
-  `TRAIN_ID` int(11) NOT NULL,
+  `TRAIN_ID` int(11) NOT NULL AUTO_INCREMENT,
   `TRAIN_NAME` varchar(50) NOT NULL,
-  `TRAIN_TYPE` varchar(50) NOT NULL,
   `TRAIN_SOURCE` varchar(50) NOT NULL,
   `TRAIN_DESTINATION` varchar(50) NOT NULL,
   `TRAIN_DEPARTURE_TIME` varchar(50) NOT NULL,
   `TRAIN_ARRIVAL_TIME` varchar(50) NOT NULL,
+  `TRAIN_DATE` DATE NOT NULL,
   `TRAIN_PRICE` int(11) NOT NULL,
-  `TRAIN_AVAILABILITY` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `TRAIN_AVAILABILITY` int(11) NOT NULL,
+  PRIMARY KEY (`TRAIN_ID`)
+);
 
 
-INSERT INTO `trains` (`TRAIN_ID`, `TRAIN_NAME`, `TRAIN_TYPE`, `TRAIN_SOURCE`, `TRAIN_DESTINATION`, `TRAIN_DEPARTURE_TIME`, `TRAIN_ARRIVAL_TIME`, `TRAIN_PRICE`, `TRAIN_AVAILABILITY`) VALUES
-(1, 'TGV', 'A', 'Lyon', 'Paris', '22:00', '13:00', 133, 24),
-(2, 'TGV', 'A', 'Marseille', 'Lyon', '21:00', '18:00', 94, 101),
-(3, 'Express', 'C', 'Paris', 'Lyon', '20:00', '08:00', 130, 154),
-(4, 'Thalys', 'C', 'Paris', 'Lille', '22:00', '14:00', 50, 90),
-(5, 'Express', 'D', 'Marseille', 'Lyon', '10:00', '13:00', 51, 133),
-(6, 'Express', 'D', 'Bordeaux', 'Lyon', '12:00', '20:00', 61, 93),
-(7, 'Thalys', 'D', 'Paris', 'Marseille', '20:00', '09:00', 132, 174),
-(8, 'Thalys', 'B', 'Lille', 'Marseille', '15:00', '13:00', 103, 167),
-(9, 'Express', 'C', 'Marseille', 'Lille', '14:00', '20:00', 59, 144),
-(10, 'TGV', 'B', 'Lille', 'Marseille', '16:00', '12:00', 104, 136),
-(11, 'TGV', 'B', 'Lyon', 'Bordeaux', '20:00', '19:00', 106, 74),
-(12, 'Express', 'B', 'Lille', 'Bordeaux', '20:00', '17:00', 118, 128),
-(13, 'Express', 'C', 'Bordeaux', 'Paris', '10:00', '12:00', 141, 28),
-(14, 'TGV', 'D', 'Bordeaux', 'Paris', '16:00', '20:00', 67, 160),
-(15, 'Express', 'B', 'Lyon', 'Marseille', '19:00', '16:00', 71, 39),
-(16, 'TER', 'A', 'Lyon', 'Lille', '16:00', '20:00', 105, 80),
-(17, 'TGV', 'A', 'Bordeaux', 'Paris', '10:00', '21:00', 109, 183),
-(18, 'Express', 'C', 'Marseille', 'Paris', '22:00', '15:00', 84, 145),
-(19, 'TGV', 'C', 'Lyon', 'Paris', '15:00', '18:00', 61, 135),
-(20, 'TER', 'A', 'Marseille', 'Lyon', '08:00', '17:00', 70, 188),
-(21, 'Thalys', 'B', 'Paris', 'Lille', '09:00', '22:00', 141, 146),
-(22, 'TER', 'C', 'Marseille', 'Lille', '08:00', '12:00', 120, 159),
-(23, 'TER', 'B', 'Bordeaux', 'Paris', '14:00', '16:00', 69, 150),
-(24, 'TGV', 'D', 'Marseille', 'Bordeaux', '18:00', '14:00', 78, 142),
-(25, 'Thalys', 'C', 'Paris', 'Lille', '10:00', '11:00', 141, 192);
-
-
-ALTER TABLE `trains`
-  ADD PRIMARY KEY (`TRAIN_ID`);
-
-
-ALTER TABLE `trains`
-  MODIFY `TRAIN_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
-COMMIT;
+INSERT INTO TRAINS (TRAIN_NAME, TRAIN_SOURCE, TRAIN_DESTINATION, TRAIN_DEPARTURE_TIME, TRAIN_ARRIVAL_TIME, TRAIN_DATE, TRAIN_PRICE, TRAIN_AVAILABILITY) VALUES
+('Thalys', 'Bordeaux', 'Marseille', '17:00', '10:00', '2022-06-09', 59, 168),
+('TER', 'Lille', 'Marseille', '10:00', '17:00', '2022-07-18', 72, 173),
+('Thalys', 'Lyon', 'Paris', '22:00', '17:00', '2022-10-15', 150, 69),
+('Express', 'Lille', 'Marseille', '16:00', '19:00', '2022-01-22', 115, 183),
+('Thalys', 'Bordeaux', 'Paris', '16:00', '20:00', '2022-06-24', 127, 39),
+('TER', 'Marseille', 'Lille', '20:00', '21:00', '2022-10-17', 105, 90),
+('TER', 'Lille', 'Paris', '11:00', '19:00', '2022-01-03', 147, 57),
+('TGV', 'Lille', 'Lyon', '09:00', '16:00', '2022-05-17', 138, 50),
+('TER', 'Bordeaux', 'Lille', '21:00', '12:00', '2022-11-11', 64, 100),
+('Express', 'Paris', 'Bordeaux', '18:00', '16:00', '2022-10-13', 105, 200),
+('Thalys', 'Paris', 'Lille', '20:00', '17:00', '2022-03-12', 145, 105),
+('Thalys', 'Lyon', 'Lille', '15:00', '15:00', '2022-03-27', 84, 178),
+('TGV', 'Bordeaux', 'Lille', '21:00', '09:00', '2022-01-24', 50, 192),
+('Thalys', 'Lille', 'Bordeaux', '10:00', '09:00', '2022-12-23', 135, 144),
+('Express', 'Lille', 'Paris', '22:00', '22:00', '2022-07-01', 92, 38),
+('Express', 'Marseille', 'Lille', '21:00', '19:00', '2022-08-20', 116, 78),
+('TGV', 'Paris', 'Bordeaux', '13:00', '08:00', '2022-05-17', 74, 141),
+('Thalys', 'Lyon', 'Bordeaux', '08:00', '12:00', '2022-03-12', 107, 109),
+('Thalys', 'Paris', 'Lyon', '13:00', '17:00', '2022-10-18', 82, 58),
+('TER', 'Paris', 'Bordeaux', '10:00', '14:00', '2022-07-05', 65, 165),
+('Express', 'Bordeaux', 'Lyon', '10:00', '22:00', '2022-03-17', 127, 22),
+('Thalys', 'Marseille', 'Paris', '08:00', '10:00', '2022-08-11', 111, 35),
+('Express', 'Marseille', 'Lille', '14:00', '13:00', '2022-03-25', 92, 88),
+('TER', 'Lille', 'Marseille', '20:00', '19:00', '2022-09-08', 104, 61),
+('Express', 'Lyon', 'Paris', '14:00', '12:00', '2022-11-09', 76, 125);
--- TrainFilteringWS/randomtraindataset.py
+++ TrainFilteringWS/randomtraindataset.py
@@ -1,23 +1,30 @@
-
 import random
-import numpy as np
 
-
-columns = ["TRAIN_ID","TRAIN_NAME","TRAIN_TYPE","TRAIN_SOURCE","TRAIN_DESTINATION","TRAIN_DEPARTURE_TIME","TRAIN_ARRIVAL_TIME","TRAIN_PRICE","TRAIN_PLACES_LEFT"]
-
-print("INSERT INTO TRAINS (TRAIN_NAME, TRAIN_TYPE, TRAIN_SOURCE, TRAIN_DESTINATION, TRAIN_DEPARTURE_TIME, TRAIN_ARRIVAL_TIME, TRAIN_PRICE, TRAIN_AVAILABILITY) VALUES")
+print("INSERT INTO TRAINS (TRAIN_NAME, TRAIN_SOURCE, TRAIN_DESTINATION, TRAIN_DEPARTURE_TIME, TRAIN_ARRIVAL_TIME, TRAIN_DATE, TRAIN_PRICE, TRAIN_AVAILABILITY) VALUES")
 for i in range(25):
+    # random date string
+    date = "2022-"
+    month = str(random.randint(1,12))
+    if len(month) == 1:
+        month = "0" + month
+    date += month + "-"
+    day = str(random.randint(1,28))
+    if len(day) == 1:
+        day = "0" + day
+    date += day
+
+
     source = random.choice(["Paris", "Lyon", "Marseille", "Bordeaux", "Lille"])
     destination = random.choice(["Paris", "Lyon", "Marseille", "Bordeaux", "Lille"])
     while source == destination:
         destination = random.choice(["Paris", "Lyon", "Marseille", "Bordeaux", "Lille"])
     print("('{}', '{}', '{}', '{}', '{}', '{}', {}, {}),".format(
         random.choice(["Express", "TGV", "TER", "Thalys"]),
-        random.choice(["A","B","C","D"]),
         source,
         destination,
         random.choice(["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]),
         random.choice(["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]),
+        date,
         random.randint(50,150),
         random.randint(10,200)
     ))
